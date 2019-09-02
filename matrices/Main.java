e33b39efc7265ad9a75984d841cbe3303edb44ee
Pedro Campos <pedrocasgcampos@live.com.pt>
2019-09-02 16:29:58 +0100

    Pass HMM 0 problem

--- matrices/Main.java
+++ matrices/Main.java
@@ -4,14 +4,29 @@ public class Main{
     System.out.println("Tests first Matrix constructer []");
 
 
-    double[] matrix1 = {1, 2};
-    Matrix m1 = new Matrix(2, 1, matrix1);
-    System.out.println("Rows    = " + m1.rows);
-    System.out.println("Columns = " + m1.columns);
-    System.out.println("MATRIX 1");
-    for(int i = 0; i < m1.rows; i++){
-      for(int j = 0; j < m1.columns; j++){
-        System.out.print(m1.data[i][j] + " ");
+    double[] matrixA = {0.2, 0.5, 0.3, 0.0, 0.1, 0.4, 0.4, 0.1, 0.2, 0.0, 0.4, 0.4, 0.2, 0.3, 0.0, 0.5};
+    Matrix mA = new Matrix(4, 4, matrixA);
+    System.out.println("Rows    = " + mA.rows);
+    System.out.println("Columns = " + mA.columns);
+    System.out.println("MATRIX A");
+    for(int i = 0; i < mA.rows; i++){
+      for(int j = 0; j < mA.columns; j++){
+        System.out.print(mA.data[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    System.out.println("\n");
+
+
+    double[] matrixB = {1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.2, 0.6, 0.2};
+    Matrix mB = new Matrix(4, 3, matrixB);
+    System.out.println("Rows    = " + mB.rows);
+    System.out.println("Columns = " + mB.columns);
+    System.out.println("MATRIX B");
+    for(int i = 0; i < mB.rows; i++){
+      for(int j = 0; j < mB.columns; j++){
+        System.out.print(mB.data[i][j] + " ");
       }
       System.out.println();
     }
@@ -21,37 +36,50 @@ public class Main{
     //Tests second Matrix constructer
     System.out.println("Tests second Matrix constructer [][]");
 
-    double[][] matrix2 = {{1}};
-    Matrix m2 = new Matrix(1, 1, matrix2);
-    System.out.println("Rows    = " + m2.rows);
-    System.out.println("Columns = " + m2.columns);
-    System.out.println("MATRIX 2");
-    for(int i = 0; i < m2.rows; i++){
-      for(int j = 0; j < m2.columns; j++){
-        System.out.print(m2.data[i][j] + " ");
+    double[][] matrixI = {{0.0, 0.0, 0.0, 1.0}};
+    Matrix mI = new Matrix(1, 4, matrixI);
+    System.out.println("Rows    = " + mI.rows);
+    System.out.println("Columns = " + mI.columns);
+    System.out.println("MATRIX I");
+    for(int i = 0; i < mI.rows; i++){
+      for(int j = 0; j < mI.columns; j++){
+        System.out.print(mI.data[i][j] + " ");
       }
       System.out.println();
     }
 
     System.out.println("\n");
 
-    //Tests vector multiplication
-    System.out.println("Tests vector multiplication");
-    System.out.println("If we multiply one row of MATRIX 1 and one column of MATRIX 2, which are vectors, the result is:");
-    System.out.println(m1.vectorMultiply(m1.getRow(0), m2.getColumn(0)));
 
-    System.out.println("\n");
+//    //Tests vector multiplication
+//    System.out.println("Tests vector multiplication");
+//    System.out.println("If we multiply one row of MATRIX 1 and one column of MATRIX 2, which are vectors, the result is:");
+//    System.out.println(mA.vectorMultiply(mB.getRow(0), mI.getColumn(0)));
+//
+//    System.out.println("\n");
 
     //Tests matrix multiplication
     System.out.println("Tests matrix multiplication");
-    System.out.println("If we multiply MATRIX 1 and MATRIX 2 the result is:");
-    Matrix res = m1.matrixMultiply(m2);
+    System.out.println("If we multiply MATRIX I and MATRIX A the result is:");
+    Matrix step = mI.matrixMultiply(mA);
+    for(int i = 0; i < step.rows; i++){
+      for(int j = 0; j < step.columns; j++){
+        System.out.print(step.data[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+
+    System.out.println("If we multiply MATRIX resultant and MATRIX B the result is:");
+    Matrix res = step.matrixMultiply(mB);
     for(int i = 0; i < res.rows; i++){
       for(int j = 0; j < res.columns; j++){
         System.out.print(res.data[i][j] + " ");
       }
       System.out.println();
     }
+
+
 
     System.out.println("Everything tested, end of test.");
 
